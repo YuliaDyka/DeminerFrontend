d6d7d0ece686a36f3d2fd82df507de2b80125fef
JuliaDyka <handsmade.yulia@gmail.com>
2025-05-25 22:57:02 +0300

    add update delete logic and fix save commands, session

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -33,9 +33,51 @@ export const save_session_request = async (commands) => {
   }
 };
 
+export const save_commands_request = async (command) => {
+  try {
+    const response = await axios.post(`${ItemsBaseURL}/commands/create`, {
+      command: command,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const update_commands_request = async (id, command) => {
+  try {
+    const response = await axios.put(`${ItemsBaseURL}/commands/${id}`, {
+      command: command,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const get_all_sessions = async () => {
   try {
     const response = await axios.get(`${ItemsBaseURL}/sessions`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getSession_by_id = async (session_id) => {
+  try {
+    const response = await axios.post(`${ItemsBaseURL}/sessions/getById`, {
+      id: session_id,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const delete_request = async (id) => {
+  try {
+    const response = await axios.delete(`${ItemsBaseURL}/commands/${id}`);
     return response.data;
   } catch (error) {
     console.error(error);
